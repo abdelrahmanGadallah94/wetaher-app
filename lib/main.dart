1421643f5c5ee5aff927936f141326810769df54
abdelrahmanGadallah94 <abhg94@yahoo.com>
2023-01-05 19:25:01 +0200

    create the routes to my application screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/views/screens/home_screen.dart';
+import 'package:weather_app/views/screens/search_screen.dart';
+import 'package:weather_app/views/settings/app_routes.dart';
 
 void main() {
   runApp(const WeatherApp());
@@ -11,6 +14,11 @@ class WeatherApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      routes: {
+        AppRoutes.homePage: (context) => const HomePage(),
+        AppRoutes.searchPage: (context) => const SearchPage(),
+      },
+      initialRoute: AppRoutes.homePage,
 
     );
   }
