c837975711515da5fa091cfbbb344ce7ff90d7fd
abdelrahmanGadallah94 <abhg94@yahoo.com>
2023-01-08 02:33:05 +0200

    update and show data using cubit

--- lib/controller/methods/search_and_update_data.dart
+++ lib/controller/methods/search_and_update_data.dart
@@ -1,27 +1,27 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import '../../views/settings/app_strings.dart';
-// import '../providers/weather_providers.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../views/settings/app_strings.dart';
+import '../providers/weather_providers.dart';
 
-// Future<void> searchAndUpdateData(BuildContext context, String? city) async {
-//   WeatherProvider provider =
-//   Provider.of<WeatherProvider>(context, listen: false);
-//
-//   await provider.getData(city: city!);
-//   provider.city = city;
-//   if(provider.weatherData == null){
-//     // ignore: use_build_context_synchronously
-//     ScaffoldMessenger.of(context).showSnackBar(
-//          const SnackBar(
-//           dismissDirection: DismissDirection.up,
-//             duration: Duration(seconds: 2),
-//             content: Text(
-//             AppStrings.error,
-//           textAlign: TextAlign.center,
-//         ))
-//     );
-//   }else{
-//     // ignore: use_build_context_synchronously
-//     Navigator.pop(context);
-//   }
-// }
+Future<void> searchAndUpdateData(BuildContext context, String? city) async {
+  WeatherProvider provider =
+  Provider.of<WeatherProvider>(context, listen: false);
+
+  await provider.getData(city: city!);
+  provider.city = city;
+  if(provider.weatherData == null){
+    // ignore: use_build_context_synchronously
+    ScaffoldMessenger.of(context).showSnackBar(
+         const SnackBar(
+          dismissDirection: DismissDirection.up,
+            duration: Duration(seconds: 2),
+            content: Text(
+            AppStrings.error,
+          textAlign: TextAlign.center,
+        ))
+    );
+  }else{
+    // ignore: use_build_context_synchronously
+    Navigator.pop(context);
+  }
+}
